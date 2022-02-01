f55b85ff78b7cdba6d9a14f97d419dbe38c95de6
pixman92 <GOtrustD@verizon.net>
2022-01-31 22:26:48 -0800

    Update windowWidthChangingListener.js

--- windowWidthChangingListener.js
+++ windowWidthChangingListener.js
@@ -1,7 +1,17 @@
 window.addEventListener('resize', function(event){
     var newWidth = window.innerWidth;
     var newHeight = window.innerHeight; 
+    changeCSS();
 });
 
 //use this in Pair with: 
-changeCSS();
+// changeCSS();
+
+function changeCSS(){
+    var widthMe = $('body').width();
+    console.log('widthMe', widthMe);
+    
+    if(widthMe>=){}
+//  OR
+    if(widthMe<=){}
+}
